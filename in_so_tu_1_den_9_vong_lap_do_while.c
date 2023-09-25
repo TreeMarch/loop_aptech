d1b1e05add5b0572c7eeee657fe6a449453a5cd5
Tran Trong Trinh <m5c0traan@gmail.com>
2023-09-25 19:43:16 +0700

    push  up to git

---
+++ in_so_tu_1_den_9_vong_lap_do_while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int tu_mot_den_chin(){
+//int main(){
+    int i;
+    i = 0;
+    do {
+        printf("i = %d \t",i);
+        i++;
+    } while (i < 11);
+}
+//}
---
+++ in_so_vong_lap_for.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//int in_so_vong_lap_for(){
+    int main(){
+    int i ;
+    for(i = 0; i < 10; i++) {
+        printf("i = %d \t", i);
+    }
+}
+//}
